ce147355e69714972b79778d19f83827c0419d5d
Cklinger <ck@novareto.de>
2015-07-13 16:25:13 +0200

    changes

--- src/nva.psyquizz/src/nva/psyquizz/browser/wizard.py
+++ src/nva.psyquizz/src/nva/psyquizz/browser/wizard.py
@@ -1,9 +1,45 @@
+# -*- coding: utf-8 -*-
+
 import uvclight 
 from uvc.entities.browser.managers import IBelowContent
+from ..models import IAccount, ICompany, ICourse, ICriterias, IClassSession
 
 
-class Wizard(uvclight.Viewlet):
+class WizardAccount(uvclight.Viewlet):
     uvclight.viewletmanager(IBelowContent)
     uvclight.name('wizard')
+    uvclight.context(IAccount)
+
+    template = uvclight.get_template('wizard_account.cpt', __file__)
+
+
+class WizardCompany(uvclight.Viewlet):
+    uvclight.viewletmanager(IBelowContent)
+    uvclight.name('wizard')
+    uvclight.context(ICompany)
 
     template = uvclight.get_template('wizard_company.cpt', __file__)
+
+
+class WizardCourse(uvclight.Viewlet):
+    uvclight.viewletmanager(IBelowContent)
+    uvclight.name('wizard')
+    uvclight.context(ICourse)
+
+    template = uvclight.get_template('wizard_course.cpt', __file__)
+
+
+class WizardCriterias(uvclight.Viewlet):
+    uvclight.viewletmanager(IBelowContent)
+    uvclight.name('wizard')
+    uvclight.context(ICriterias)
+
+    template = uvclight.get_template('wizard_criterias.cpt', __file__)
+
+
+class WizardSession(uvclight.Viewlet):
+    uvclight.viewletmanager(IBelowContent)
+    uvclight.name('wizard')
+    uvclight.context(IClassSession)
+
+    template = uvclight.get_template('wizard_session.cpt', __file__)
