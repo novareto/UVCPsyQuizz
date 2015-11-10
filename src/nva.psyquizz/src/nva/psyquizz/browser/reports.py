50413af535a18c996a17c2320b7e6d7e67adfc20
Cklinger <ck@novareto.de>
2015-11-10 07:50:45 +0100

    chagns

---
+++ src/nva.psyquizz/src/nva/psyquizz/browser/reports.py
@@ -0,0 +1,45 @@
+import uvclight
+from cStringIO import StringIO
+from zope.interface import Interface
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Image, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from tempfile import NamedTemporaryFile
+from binascii import a2b_base64
+
+styles = getSampleStyleSheet()
+
+
+def read_data_uri(uri):
+    ctype, data = uri.split(',', 1)
+    binary_data = a2b_base64(data)
+    fd = StringIO()
+    fd.write(binary_data)
+    fd.seek(0)
+    return fd
+
+
+class GeneratePDF(uvclight.Page):
+    uvclight.context(Interface)
+    uvclight.name('pdf')
+    uvclight.auth.require('zope.Public')
+
+    def make_response(self, result):
+        response = self.responseFactory(app_iter=result)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'attachment; filename="charts.pdf"'
+        return response
+
+    def render(self):
+        doc = SimpleDocTemplate(NamedTemporaryFile(), pagesize=letter)
+        parts = []
+
+        userschart = read_data_uri(self.request.form['userschart'])
+
+        parts.append(Paragraph(u'HALLO WELT', styles['Normal']))
+        parts.append(Image(userschart, width=600, height=600))
+
+        doc.build(parts)
+        pdf = doc.filename
+        pdf.seek(0)
+        return pdf.read()
