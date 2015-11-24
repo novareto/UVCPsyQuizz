b4b2dd42055c05354c6d9f3f806423d1eb15d36f
Christian Klinger <ck@novareto.de>
2015-11-24 11:16:20 +0100

    checking the caluctaions of the results

--- src/nva.psyquizz/src/nva/psyquizz/models/vocabularies.py
+++ src/nva.psyquizz/src/nva/psyquizz/models/vocabularies.py
@@ -25,7 +25,7 @@ LessToMore = SimpleVocabulary([
     ])
 
 
-MoreToLess = SimpleVocabulary([
+MoreToLessN = SimpleVocabulary([
     SimpleTerm(value=5,
                title=u'trifft gar nicht zu'),
     SimpleTerm(value=4,
@@ -35,6 +35,19 @@ MoreToLess = SimpleVocabulary([
     SimpleTerm(value=2,
                title=u'trifft überwiegend zu'),
     SimpleTerm(value=1,
+               title=u'trifft völlig zu'),
+    ])
+
+MoreToLess = SimpleVocabulary([
+    SimpleTerm(value=1,
+               title=u'trifft gar nicht zu'),
+    SimpleTerm(value=2,
+               title=u'trifft wenig zu'),
+    SimpleTerm(value=3,
+               title=u'trifft mittelmäßig zu'),
+    SimpleTerm(value=4,
+               title=u'trifft überwiegend zu'),
+    SimpleTerm(value=5,
                title=u'trifft völlig zu'),
     ])
 
